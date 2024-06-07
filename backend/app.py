92c2bb0c0cbf82d720ffd86b9f925757d83d3b0a
Maggie <103873589+maggiexqc@users.noreply.github.com>
2024-06-07 14:46:13 -0700

    Load in front page

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,32 @@
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__, static_folder='../docs', static_url_path='')
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-    
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from model import predict  # Import your model loading and prediction functions here
-
-app = Flask(__name__)
-CORS(app)  # Enable CORS if you need cross-origin requests from your frontend
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Assume an image file is sent in the request
-    image = request.files['file']
-    # Process the image with your CNN
-    prediction = predict(image)
-    return jsonify({'prediction': prediction})
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('../docs', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# from model import predict  # Import your model loading and prediction functions here
+
+# app = Flask(__name__)
+# CORS(app)  # Enable CORS if you need cross-origin requests from your frontend
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     # Assume an image file is sent in the request
+#     image = request.files['file']
+#     # Process the image with your CNN
+#     prediction = predict(image)
+#     return jsonify({'prediction': prediction})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
