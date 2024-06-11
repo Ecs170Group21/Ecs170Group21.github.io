96396bdd44cc01893f20aceb9367f419631cd601
Arvind-t33 <arvindtawker@gmail.com>
2024-06-11 14:55:01 -0700

    completed dynamic front-end and back-end interaction

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = {'mat'}
 MODEL_PATH = 'base_classifier/'
+ALL_LABELS = {0: 'Meningioma', 1: 'Glioma', 2: 'Pituitary tumor'}
 
 app = Flask(__name__, template_folder='docs', static_folder='docs', static_url_path='')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -71,19 +72,45 @@ def upload_file():
 def process_image(image_path):
     # Load and process the .mat v7.3 file using h5py
     with h5py.File(image_path, 'r') as f:
-        data = preprocess(f)
-        result = model_inference(data)
-        return result
+        # Convert the HDF5 file to a .mat file
+        mat_data = {}
+        def recursively_add_keys(group, dict_obj):
+            for key in group.keys():
+                item = group[key]
+                if isinstance(item, h5py.Dataset):
+                    dict_obj[key] = item[()]
+                elif isinstance(item, h5py.Group):
+                    dict_obj[key] = {}
+                    recursively_add_keys(item, dict_obj[key])
+        
+        recursively_add_keys(f, mat_data)
+        
+        mat_filepath = image_path.replace('.h5', '.mat')
+        sio.savemat(mat_filepath, mat_data)
+        print(f"HDF5 to .mat conversion complete. Saved at {mat_filepath}")
+        # Load the .mat file
+        mat_file = sio.loadmat(mat_filepath)
+        print("Structure of the .mat file:")
+        for key in mat_file.keys():
+            print(f"Key: {key}, Type: {type(mat_file[key])}")
+        data = preprocess(mat_file)
+    # further processing...
+    prediction = model_inference(data)
+    return f'Prediction: {ALL_LABELS[prediction]}'
 
-# TODO
 def preprocess(data):
-    # Implement your preprocessing here
-    # Example: assuming 'data' is the key for the dataset in the .mat file
-    datum = data['image']
+    cjdata = data['cjdata']
+    
+    if 'image' not in cjdata.dtype.names:
+        raise KeyError("Object 'image' not found in the 'cjdata' structure")
+    
+    # Accessing the image data within the cjdata structure
+    datum = np.array(cjdata['image'][0,0])
     processed_data = datum / 12728  # Example normalization
+    processed_data = np.expand_dims(processed_data, axis=(0,-1))
+
     return processed_data
 
-# TODO
 def model_inference(data):
     # Convert data to the format expected by the model and run inference
     predictions = model.predict(data)
@@ -91,5 +118,5 @@ def model_inference(data):
     return prediction
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0', port=port)
